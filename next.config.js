fb6fcb6d9e70b8d0bedb37a99519f630b3a4cc2e
together320 <yuzheli320@gmail.com>
2023-10-31 10:05:57 +0800

    hopefully.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  output: 'export',
   reactStrictMode: true,
   generateBuildId: async () => {
     // You can, for example, get the latest git commit hash here
-    return 'my-build-id'
+    return 'my-build-id';
   },
   images: {
     unoptimized: true,
-    domains : ['img.icons8.com'],
+    domains: ['img.icons8.com'],
     remotePatterns: [
       {
         protocol: 'https',
